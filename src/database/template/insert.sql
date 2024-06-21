0c0b05b82d7053be64a70946501ee8188681cf54
italomsilva <italoroyale13@gmail.com>
2024-06-21 17:09:51 -0300

    update insert sql

--- src/database/template/insert.sql
+++ src/database/template/insert.sql
@@ -1,12 +1,12 @@
 -- Inserir usuários na tabela users
 INSERT INTO users (id, name, email, password, phone, created_at, updated_at, auth_token) 
 VALUES 
-  ('1', 'John Doe', 'john.doe@example.com', 'senha123', '123456789', NOW(), NOW(), NULL),
-  ('2', 'Jane Smith', 'jane.smith@example.com', 'senha456', NULL, NOW(), NOW(), NULL),
-  ('3', 'Michael Johnson', 'michael.johnson@example.com', 'senha789', '987654321', NOW(), NOW(), NULL);
+  ('1', 'Lilith Langley', 'asuka.red02@example.com', 'unidade2', '123456789', NOW(), NOW(), NULL),
+  ('2', 'adão Ikari', 'shinji.pruple01@example.com', 'unidade1', '987654321', NOW(), NOW(), NULL),
+  ('3', 'Eva Ayanami', 'rey.blue00@example.com', 'unidade0', NULL, NOW(), NOW(), NULL);
 
 -- Inserir Task
-INSERT INTO tasks (id, title, description, duration, type, owner_user, created_at) VALUES
-('task_1', 'First Task', 'This is the first task', 120, 'Development', '1', CURRENT_TIMESTAMP),
-('task_2', 'Second Task', 'This is the second task', 90, 'Testing', '1', CURRENT_TIMESTAMP),
-('task_3', 'Third Task', 'This is the third task', 45, 'Documentation', '2', CURRENT_TIMESTAMP);
+INSERT INTO tasks (id, title, description, duration, type, owner_user, created_at, completed) VALUES
+('task_1', 'First Task', 'This is the first task', 120, 'Development', '1', CURRENT_TIMESTAMP, FALSE),
+('task_2', 'Second Task', 'This is the second task', 90, 'Testing', '2', CURRENT_TIMESTAMP, FALSE),
+('task_3', 'Third Task', 'This is the third task', 45, 'Documentation', '3', CURRENT_TIMESTAMP, FALSE);
